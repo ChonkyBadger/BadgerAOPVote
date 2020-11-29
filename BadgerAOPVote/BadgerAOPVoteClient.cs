22b9c565c71382b3cb73a5a00257cfeabca14438
Badger <unconfigured@null.spigotmc.org>
2020-11-29 17:30:56 +0000

    Add project files.

---
+++ BadgerAOPVote/BadgerAOPVoteClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+using System.Linq;
+
+namespace BadgerAOPVote
+{
+    public class BadgerAOPVoteClient : BaseScript
+    {
+        bool aopVoteActive = false;
+        bool canVote = false;
+        int maxOptions = 0;
+
+        public BadgerAOPVoteClient()
+		{
+
+            EventHandlers["BadgerAOPVote:StartAOPVote"] += new Action<int, string, string, string, int>(StartVote);
+            EventHandlers["BadgerAOPVote:EndVote"] += new Action<bool, string, string, string>(EndVote);
+
+            //
+            // Commands
+            //
+
+            // Vote for an aop
+            RegisterCommand("vote", new Action<int, List<object>, string>((source, args, raw) =>
+            {
+                Debug.WriteLine("vote command");
+
+                // If there is an active vote
+                if (aopVoteActive)
+                {
+                    if (canVote)
+                    {
+                        int vote = int.Parse(args[0].ToString());
+
+                        // If vote is invalid
+                        if (vote > maxOptions || vote <= 0)
+                        {
+                            Screen.ShowNotification("There is no option " + vote);
+                        }
+                        else
+                        {
+                            TriggerServerEvent("BadgerAOPVote:SendVoteToServer", vote);
+                            canVote = false;
+                            Screen.ShowNotification("Your vote has been recorded");
+                        }
+                    }
+                    else
+                    {
+                        Screen.ShowNotification("You have already voted!");
+                    }
+                }
+                else 
+                {
+                    Screen.ShowNotification("No AOP Vote is currently active");
+                }
+            }), false);
+        }
+
+        private void StartVote(int maxOption, string options, string colour1, string colour2, int timeToVote)
+		{
+            maxOptions = maxOption;
+            aopVoteActive = true;
+            canVote = true;
+
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 255, 0, 0 },
+                multiline = true,
+                args = new[] { colour2 + "An " + colour1 + "AOP Vote " + colour2 + "has been started! " + 
+                "You have " + colour1 + timeToVote + " seconds " + colour2 + "to vote using " + colour1 +  "/vote <option> " + "\n" +
+                options }
+            });
+        }
+
+        private void EndVote(bool noVotes, string aop, string colour1, string colour2)
+		{
+            canVote = false;
+            aopVoteActive = false;
+
+            if (noVotes)
+            {
+                TriggerEvent("chat:addMessage", new
+                {
+                    color = new[] { 255, 0, 0 },
+                    multiline = true,
+                    args = new[] { colour1 + "AOP Vote " + colour2 + "has ended without any votes" }
+                });
+            }
+            else
+            {
+                TriggerEvent("chat:addMessage", new
+                {
+                    color = new[] { 255, 0, 0 },
+                    multiline = true,
+                    args = new[] { colour1 + "AOP Vote " + colour2 + "has ended! " + "The winning aop is " + colour1 + aop }
+                });
+            }
+        }
+    }
+}
---
+++ BadgerAOPVoteServer/BadgerAOPVoteServer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using System.Linq;
+
+namespace BadgerAOPVoteServer
+{
+    public class BadgerAOPVoteServer : BaseScript
+    {
+        string jsonConfig = LoadResourceFile(GetCurrentResourceName(), "config/config.json");
+
+        bool badgerEssentialsLink;
+        int aopVoteDuration;
+        string colour1;
+        string colour2;
+
+        List<string> voteOptions = new List<string>();
+        List<int> voteList = new List<int>();
+        string voteOptionsMsg;
+        int maxOptions;
+
+        public BadgerAOPVoteServer()
+        {
+            Tick += OnTick;
+
+            JObject o = JObject.Parse(jsonConfig);
+
+            badgerEssentialsLink = (bool)o.SelectToken("badgerAOPVote.badgerEssentialsLink");
+            colour1 = (string)o.SelectToken("badgerAOPVote.colour1");
+            colour2 = (string)o.SelectToken("badgerAOPVote.colour2");
+
+            EventHandlers["BadgerAOPVote:SendVoteToServer"] += new Action<int>(ReceiveVote);
+
+            //
+            // Commands
+            //
+
+            // Start an aop vote
+            RegisterCommand("startVote", new Action<int, List<object>, string>((source, args, raw) =>
+            {
+                // arg 0 = duration
+                // arg 1+ = options
+                voteOptions.Clear();
+                voteList.Clear();
+
+                object[] argArray = args.ToArray();
+                maxOptions = args.Count - 1;
+                voteOptionsMsg = string.Empty;
+
+                int index = 0;
+                foreach (string i in argArray)
+                {
+                    if (index > 0) // index 0 is for duration
+                    {
+                        voteOptions.Add(i);
+                        voteOptionsMsg += colour1 + "Option " + index + ": " + colour2 + i + "\n";
+                    }
+
+                    index++;
+                }
+
+                aopVoteDuration = int.Parse(argArray[0].ToString());
+                TriggerClientEvent("BadgerAOPVote:StartAOPVote", maxOptions, voteOptionsMsg, colour1, colour2, aopVoteDuration);
+            }), false);
+
+        }
+
+        private async Task OnTick()
+        {
+            if (aopVoteDuration > 0)
+            {
+                await Delay(1000);
+                aopVoteDuration--;
+                if (aopVoteDuration <= 0)
+                {
+                    EndAOPVote();
+                }
+            }
+        }
+
+        private void SendAOPToBadgerEssentials(string aop)
+        {
+            TriggerEvent("BadgerEssentials:GetAOPFromBadgerAOPVote", aop);
+        }
+
+        private void ReceiveVote(int vote)
+        {
+            voteList.Add(vote);
+        }
+
+        private void EndAOPVote()
+		{
+            if (voteList.Count > 0)
+			{
+                int most = voteList.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
+                string aop = voteOptions[most - 1];
+
+                TriggerClientEvent("BadgerAOPVote:EndVote", false, aop, colour1, colour2);
+
+                if (badgerEssentialsLink)
+                {
+                    SendAOPToBadgerEssentials(aop);
+                }
+            }
+            else // No Votes Received
+			{
+
+                TriggerClientEvent("BadgerAOPVote:EndVote", true, string.Empty, colour1, colour2);
+            }
+		}
+    }
+}
